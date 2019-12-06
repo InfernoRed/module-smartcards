73115084f3acb56bf77d4dbcc17a4e5bcbb68a1a
Matt <github@addressxception.com>
2019-12-06 08:43:39 -0500

    Use jsonify for returning results.
    binary decode into objects instead of strings using json.loads.  Add postman collection and environment for interacting with the API

--- smartcards/core.py
+++ smartcards/core.py
@@ -3,7 +3,7 @@ import json
 import os
 import base64
 
-from flask import Flask, send_from_directory, request
+from flask import Flask, send_from_directory, request, jsonify
 
 from smartcard.System import readers
 from smartcard.util import toHexString, toASCIIBytes, toASCIIString
@@ -22,36 +22,36 @@ app = Flask(__name__)
 def card_read():
     card_bytes, long_value_exists = CardInterface.read()
     if card_bytes is None:
-        return json.dumps({"present": False})
+        return jsonify({"present": False})
 
     card_data = card_bytes.decode('utf-8')
     if card_data:
-        return json.dumps({"present": True, "shortValue": card_data, "longValueExists": long_value_exists})
+        return jsonify({"present": True, "shortValue": json.loads(card_data), "longValueExists": long_value_exists})
     else:
-        return json.dumps({"present": True})
+        return jsonify({"present": True})
 
 
 @app.route('/card/read_long')
 def card_read_long():
     long_bytes = CardInterface.read_long()
     if long_bytes:
-        return json.dumps({"longValue": long_bytes.decode('utf-8')})
+        return jsonify(json.loads(long_bytes.decode('utf-8')))
     else:
-        return json.dumps({})
+        return jsonify({})
 
 
 @app.route('/card/write', methods=["POST"])
 def card_write():
     content = request.data
     rv = CardInterface.write(content)
-    return json.dumps({"success": rv})
+    return jsonify({"success": rv})
 
 
 @app.route('/card/write_and_protect', methods=["POST"])
 def card_write_and_protect():
     content = request.data
     rv = CardInterface.write(content, write_protect=True)
-    return json.dumps({"success": rv})
+    return jsonify({"success": rv})
 
 
 @app.route('/card/write_short_and_long', methods=["POST"])
@@ -60,29 +60,29 @@ def card_write_short_and_long():
     long_value = request.form['long_value']
     rv = CardInterface.write_short_and_long(
         short_value.encode('utf-8'), long_value.encode('utf-8'))
-    return json.dumps({"success": rv})
+    return jsonify({"success": rv})
 
 
 @app.route('/card/read_long_b64', methods=["GET"])
 def card_read_long_b64():
     long_bytes = CardInterface.read_long()
     if long_bytes:
-        return json.dumps({"longValue": base64.b64encode(long_bytes).decode('ascii')})
+        return jsonify({"longValue": base64.b64encode(long_bytes).decode('ascii')})
     else:
-        return json.dumps({"longValue": None})
+        return jsonify({"longValue": None})
 
 
 @app.route('/card/write_long_b64', methods=["POST"])
 def card_write_long_b64():
     long_value = request.form["long_value"]
     rv = CardInterface.write_long(base64.b64decode(long_value))
-    return json.dumps({"success": rv})
+    return jsonify({"success": rv})
 
 
 @app.route('/card/write_protect_override', methods=["POST"])
 def card_write_protect_override():
     CardInterface.override_protection()
-    return json.dumps({"success": True})
+    return jsonify({"success": True})
 
 
 @app.route('/')
